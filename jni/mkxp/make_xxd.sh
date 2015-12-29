432dbdf1e865ed8ec1274f6c8469e41f1893bf3f
xperia64 <xperiancedapps@gmail.com>
2015-12-28 23:34:40 -0500

    Fix removing duplicate xxd files

--- jni/mkxp/make_xxd.sh
+++ jni/mkxp/make_xxd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-rm "shader/*.xxd*"
+rm shader/*.xxd*
 for i in $(ls shader); do
 	xxd -i "shader/$i" "shader/$i.xxd"
 done
-rm "assets/*.xxd*"
+rm assets/*.xxd*
 for i in $(ls assets); do
 	xxd -i "assets/$i" "assets/$i.xxd"
 done
