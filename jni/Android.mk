f28f06dacd82fb1671cb523ec78b092f3566cd54
RadialApps <radialapps@gmail.com>
2017-08-10 23:24:59 +0530

    Remove pixman

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,7 @@
 #LOCAL_PATH:= $(call my-dir)
 
 L_PATH := $(call my-dir)
+include $(L_PATH)/pixman.mk
 include $(L_PATH)/SDL.mk
 include $(L_PATH)/SDL_ttf.mk
 include $(L_PATH)/boost.program_options/Android.mk
@@ -11,7 +12,6 @@ include $(L_PATH)/mkxp/Android.mk
 include $(L_PATH)/mkxp-wrapper/Android.mk
 include $(L_PATH)/OpenAL/Android.mk
 include $(L_PATH)/physfs/Android.mk
-include $(L_PATH)/pixman/Android.mk
 include $(L_PATH)/ruby/Android.mk
 include $(L_PATH)/SDL_image.mk
 include $(L_PATH)/SDL_sound/Android.mk
